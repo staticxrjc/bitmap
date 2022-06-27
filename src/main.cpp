f7e2230afc617d518e30124224ca54028a3b5c17
staticxrjc <staticxrjc@gmail.com>
2022-06-26 17:01:12 -0700

    Lots of changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,28 @@
 #include <bit>
 #pragma pack(1)
 
-struct FirstHeader {
+#ifndef __LITTLE_ENDIAN__
+	#ifndef __BIG_ENDIAN__
+		#define __LITTLE_ENDIAN__
+	#endif
+#endif
+
+#ifdef __LITTLE_ENDIAN__
+	#define BITMAP_SIGNATURE 0x4d42
+#else
+	#define BITMAP_SIGNATURE 0x424d
+#endif
+
+namespace bmp {
+
+struct FileHeader {
     uint16_t signature;
     uint32_t fileSize;
     uint32_t reserved;
     uint32_t dataOffset;
 };
 
-struct SecondHeader {
+struct InfoHeader {
     uint32_t headerSize;
     uint32_t imageWidth;
     uint32_t imageHeight;
@@ -21,20 +35,54 @@ struct SecondHeader {
     uint32_t imageSize;
     uint32_t xPixelsPerMeter;
     uint32_t yPixelsPerMeter;
+    uint32_t colorsInColorTable;
+    uint32_t importantColorCount;
+    uint32_t redChannelBitmask;
+    uint32_t greenChannelBitmask;
+    uint32_t blueChannelBitmask;
+    uint32_t alphaChannelBitmask;
+    uint32_t colorSpaceType;
+    uint32_t colorSpaceEndpoints[9];
+    uint32_t gammaForRedChannel;
+    uint32_t gammaForGreenChannel;
+    uint32_t gammaForBlueChannel;
+    uint32_t intent;
+    uint32_t iccProfileData;
+    uint32_t iccProfileSize;
+    uint32_t Reserved;
 };
 
+typedef struct pixel {
+    uint alpha;
+    uint green;
+    uint red;
+    uint blue;
+} pixel;
+
+}
+
 int main() {
+    using namespace bmp;
+
     std::fstream file;
     file.open("../data/test.bmp",std::ios_base::in);
     if(file.is_open()) {
-        char header1[sizeof(FirstHeader)];
-        char header2[sizeof(SecondHeader)];
+        // Grab 1st Header
+        char header1[sizeof(FileHeader)];
         file.read(header1,sizeof(header1));
+        auto H1 = std::bit_cast<FileHeader>(header1);
+        int h2Size = H1.dataOffset - sizeof(FileHeader);
+        std::cout << "BM Str: " << std::hex << BITMAP_SIGNATURE << std::endl;
+        if(BITMAP_SIGNATURE == H1.signature)
+            std::cout << "Bitmap Image" << std::endl;
+        else
+            std::cout << "Not bitmap Image" << std::endl;
+        printf("H2 Size: %d\n",h2Size);
+
+        // Grab 2nd Header
+        char header2[sizeof(InfoHeader)];
         file.read(header2,sizeof(header2));
-        auto H1 = std::bit_cast<FirstHeader>(header1);
-        auto H2 = std::bit_cast<SecondHeader>(header2);
-        std::cout << "header1 raw " << header1 << std::endl;
-        std::cout << "Signature: " << (char)(H1.signature & 0xFF) << (char)(H1.signature>>8) << std::endl;
+        auto H2 = std::bit_cast<InfoHeader>(header2);
         std::cout << "File Size: " << H1.fileSize << std::endl;
         std::cout << "Data Offset: " << H1.dataOffset << std::endl;
         std::cout << "----------" << std::endl;
@@ -47,6 +95,42 @@ int main() {
         std::cout << "Image Size: " << H2.imageSize << std::endl;
         std::cout << "X Pixels Per Meter: " << H2.xPixelsPerMeter << std::endl;
         std::cout << "Y Pixels Per Meter: " << H2.yPixelsPerMeter << std::endl;
+        std::cout << "Colors in Color Table: " << H2.colorsInColorTable << std::endl;
+        std::cout << "Importang Color Count: " << H2.importantColorCount << std::endl;
+        std::cout << "Red Channel Bitmask: 0x" << std::hex << H2.redChannelBitmask << std::endl;
+        std::cout << "Green Channel Bitmask: 0x" << std::hex << H2.greenChannelBitmask << std::endl;
+        std::cout << "Blue Channel Bitmask: 0x" << std::hex << H2.blueChannelBitmask << std::endl;
+        std::cout << "Alpha Channel Bitmask: 0x" << std::hex << H2.alphaChannelBitmask << std::endl;
+        std::cout << "Color Space Type: 0x" << std::hex << H2.colorSpaceType << std::endl;
+        
+        switch (H2.bitsPerPixel) {
+            case 16: {
+                char pixel[2];
+                uint16_t pixels;
+
+                for(int i = H2.imageHeight; i > 0; i--) {
+                    for(int j = 0; j < H2.imageWidth; j++) {
+                        file.read(pixel, 2);
+                        pixels = std::bit_cast<uint16_t>(pixel);
+                        std::cout << "Pixels: " << std::hex << pixels << std::endl;
+                        uint16_t red = pixels & H2.redChannelBitmask;
+                        red = red >> 8;
+                        uint16_t green = pixels & H2.greenChannelBitmask;
+                        green = green >> 4;
+                        std::cout << "Red: " << red << std::endl;
+                        std::cout << "Green: " << green << std::endl;
+                    }
+                }
+                
+                break;
+                }
+            default:
+                std::cout << "Bits per Pixel: " << H2.bitsPerPixel << " not supported!" << std::endl;
+                break;
+        }
     }
+    
     file.close();
+
+    return 1;
 }
